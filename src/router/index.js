2f0ebee59d841fce2ff17051550ade609ebff5e9
Uliana Blinova <ulanablinova0@gmail.com>
2026-03-15 11:15:36 +0700

    add router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,46 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import store from '@/store'
+
+const requireAuth = (to, from, next) => {
+  if (store.getters.isAuthenticated) {
+    next();
+  } else {
+    next('/login');
+  }
+};
+
+const requireGuest = (to, from, next) => {
+  if (!store.getters.isAuthenticated) {
+    next();
+  } else {
+    next('/');
+  }
+};
 
 const routes = [
+  {
+    path: '/',
+    name: 'Catalog',
+    component: () => import('@/views/CatalogView.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('@/views/LoginView.vue'),
+    beforeEnter: requireGuest
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import('@/views/RegisterView.vue'),
+    beforeEnter: requireGuest
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: () => import('@/views/CartView.vue'),
+    beforeEnter: requireAuth
+  }
 ]
 
 const router = createRouter({
